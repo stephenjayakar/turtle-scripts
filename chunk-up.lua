a52522a1093d3ae77be9b57b737c36c1a8de90b2
Stephen Jayakar <aj12ay@gmail.com>
2024-04-03 17:25:57 -0700

    go forward and back with goto abstraction

--- chunk-up.lua
+++ chunk-up.lua
@@ -1,7 +1,51 @@
-local yLimit = 20 -- Define the y limit for the script
-local startFuelLevel = turtle.getFuelLevel()
+-- Turtle mocks
+local turtle = {
+    inspect = function()
+        -- Return a mock result, true if there is a "chest" (assuming a specific block ID)
+        return true
+    end,
+    turnLeft = function()
+    end,
+    turnRight = function()
+    end,
+    forward = function()
+        -- Assume the turtle moves forward successfully
+        return true
+    end,
+    dig = function()
+        -- Assume the turtle digs successfully
+        return true
+    end,
+    getFuelLevel = function()
+        -- Return a mock fuel level
+        return 1000
+    end
+    -- Add more turtle methods as needed for your script
+}
+-- end turtle mocks
 
--- Check if there's a chest behind the turtle to begin with
+
+-- constants
+------------
+local yLimit = 20 -- Define the y limit for the script
+local dim = 3
+-- assumes turning right
+local xTable = {0, 1, 0, -1}
+local zTable = {1, 0, -1, 0}
+
+local startFuelLevel = turtle.getFuelLevel()
+local x, y, z = 0, 0, 0
+-- Array indexes start at 1 dear Lord
+local xDirIndex,zDirIndex = 1, 1
+
+local function xDir()
+   return xTable[xDirIndex]
+end
+
+local function zDir()
+   return zTable[zDirIndex]
+end
+
 local function checkChest()
   turtle.turnLeft()
   turtle.turnLeft()
@@ -11,102 +55,53 @@ local function checkChest()
   return result
 end
 
--- Function to drop all items except fuel
-local function dropItems()
-  for i=1,16 do
-    turtle.select(i)
-    if not turtle.refuel(0) then -- If it's not fuel
-      turtle.drop() -- Drop the item
-    end
-  end
+local function turnRight()
+   turtle.turnRight()
+   xDirIndex = xDirIndex % 4 + 1
+   zDirIndex = zDirIndex % 4 + 1
 end
 
--- Unload items to the chest behind
-local function unloadToChest()
-  turtle.turnLeft()
-  turtle.turnLeft()
-  dropItems()
-  turtle.turnLeft()
-  turtle.turnLeft()
+local function moveForward()
+   if turtle.inspect() then
+      turtle.dig()
+   end
+   turtle.forward()
+   x = x + xDir()
+   z = z + zDir()
 end
 
--- Refuel from inventory or chest if needed
-local function refuelIfNeeded()
-  if turtle.getFuelLevel() < (turtle.getFuelLimit() - startFuelLevel) / 2 then
-    if not turtle.refuel() then
-      unloadToChest()
-      turtle.suck(startFuelLevel / 2) -- Get coal out of the chest
-      turtle.refuel()
-    end
-  end
-end
-
--- Check for space in the inventory and refuel if needed
-local function manageInventory()
-  local itemSpace = 0
-  for i=1,16 do
-    if turtle.getItemCount(i) == 0 then
-      itemSpace = itemSpace + 1
-    end
-  end
-  if itemSpace < 2 then -- If less than 2 slots are free, go unload
-    unloadToChest()
-  end
-  refuelIfNeeded()
-end
-
--- Mine in an upward 8x8 block
-local function mineUpwards()
-  for y=1,yLimit do
-    for i=1,8 do
-      for j=1,7 do -- We go to 7 because we move forward first before starting to mine
-        if not turtle.forward() then
-          turtle.dig()
-          turtle.forward()
-        end
-        manageInventory()
+local function goto(newX, newZ)
+   local xDel = newX - x
+   local zDel = newZ - z
+   print("deltas", xDel, zDel)
+   print("dir", xDir(), zDir())
+   -- Do z movement first
+   if zDel > 0 then
+      while zDir() ~= 1 do
+         turnRight()
       end
-      if i < 8 then -- On the last column, don't turn
-        if i % 2 == 1 then -- Zigzag pattern
-          turtle.turnRight()
-          if not turtle.forward() then
-            turtle.dig()
-            turtle.forward()
-          end
-          manageInventory()
-          turtle.turnRight()
-        else
-          turtle.turnLeft()
-          if not turtle.forward() then
-            turtle.dig()
-            turtle.forward()
-          end
-          manageInventory()
-          turtle.turnLeft()
-        end
+   end
+   if zDel < 0 then
+      while zDir() ~= -1 do
+         turnRight()
       end
-    end
-    turtle.digUp()
-    turtle.up()
-    manageInventory()
-    if y % 2 == 0 then -- After going up, turn around if needed to continue the pattern
-      turtle.turnRight()
-      turtle.turnRight()
-    end
-  end
+   end
+   -- Now it's facing the right way
+   for n=1,math.abs(zDel) do
+      moveForward()
+   end
 end
 
--- Main function to execute the program
 local function main()
+  -- Make sure there's a chest behind
   if not checkChest() then
     print("Please place a chest behind the turtle to begin.")
     return
   end
-  mineUpwards()
-  -- Return to the starting position at the bottom
-  for y=1,yLimit do
-    turtle.down()
-  end
+
+  -- This function just goes forward and back 4 blocks
+  goto(0, 4)
+  goto(0, 0)
 end
 
-main() -- Run the main function
+main()
