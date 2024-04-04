1eae30072d147e558471b55a36a54974996ab53c
Stephen Jayakar <aj12ay@gmail.com>
2024-04-04 16:38:41 -0700

    Made turning more optimal

--- chunk-up.lua
+++ chunk-up.lua
@@ -21,10 +21,7 @@
         return 1000
     end
     -- Add more turtle methods as needed for your script
-} ]]
--- end turtle mocks
-
-
+} ]] -- end turtle mocks
 -- constants
 ------------
 local yLimit = 20 -- Define the y limit for the script
@@ -36,119 +33,143 @@ local zTable = {1, 0, -1, 0}
 local startFuelLevel = turtle.getFuelLevel()
 local x, y, z = 0, 0, 0
 -- Array indexes start at 1 dear Lord
-local xDirIndex,zDirIndex = 1, 1
+local xDirIndex, zDirIndex = 1, 1
 
 local function xDir()
-   return xTable[xDirIndex]
+    return xTable[xDirIndex]
 end
 
 local function zDir()
-   return zTable[zDirIndex]
+    return zTable[zDirIndex]
 end
 
 local function checkChest()
-  turtle.turnLeft()
-  turtle.turnLeft()
-  local result = turtle.inspect()
-  turtle.turnRight()
-  turtle.turnRight()
-  return result
+    turtle.turnLeft()
+    turtle.turnLeft()
+    local result = turtle.inspect()
+    turtle.turnRight()
+    turtle.turnRight()
+    return result
 end
 
 local function turnRight()
-   turtle.turnRight()
-   xDirIndex = xDirIndex % 4 + 1
-   zDirIndex = zDirIndex % 4 + 1
+    turtle.turnRight()
+    xDirIndex = xDirIndex % 4 + 1
+    zDirIndex = zDirIndex % 4 + 1
+end
+
+local function turnLeft()
+    turtle.turnLeft()
+    xDirIndex = (xDirIndex - 2) % 4 + 1
+    zDirIndex = (zDirIndex - 2) % 4 + 1
 end
 
 local function moveForward()
-   if turtle.inspect() then
-      turtle.dig()
-   end
-   turtle.forward()
-   x = x + xDir()
-   z = z + zDir()
+    if turtle.inspect() then
+        turtle.dig()
+    end
+    turtle.forward()
+    x = x + xDir()
+    z = z + zDir()
 end
 
 local function moveUp()
-   if turtle.inspectUp() then
-      turtle.digUp()
-   end
-   turtle.up()
-   y = y + 1
+    if turtle.inspectUp() then
+        turtle.digUp()
+    end
+    turtle.up()
+    y = y + 1
 end
 local function moveDown()
-   if turtle.inspectDown() then
-      turtle.digDown()
-   end
-   turtle.down()
-   y = y - 1
+    if turtle.inspectDown() then
+        turtle.digDown()
+    end
+    turtle.down()
+    y = y - 1
 end
 
 local function moveTo(newX, newZ)
-   local xDel = newX - x
-   local zDel = newZ - z
-   print("deltas", xDel, zDel)
-   print("dir", xDir(), zDir())
-   -- Do z movement first
-   if zDel > 0 then
-      while zDir() ~= 1 do
-         turnRight()
-      end
-   end
-   if zDel < 0 then
-      while zDir() ~= -1 do
-         turnRight()
-      end
-   end
-   -- Now it's facing the right way
-   for n=1,math.abs(zDel) do
-      moveForward()
-   end
+    local xDel = newX - x
+    local zDel = newZ - z
+    print("deltas", xDel, zDel)
+    print("dir", xDir(), zDir())
+    -- Do z movement first
+    if zDel > 0 then
+        -- left turn optimization
+        if zDirIndex == 2 then
+            turnLeft()
+        else
+            while zDir() ~= 1 do
+                turnRight()
+            end
+        end
+    end
+    if zDel < 0 then
+        -- left turn optimization
+        if zDirIndex == 4 then
+            turnLeft()
+        else
+            while zDir() ~= -1 do
+                turnRight()
+            end
+        end
+    end
+    -- Now it's facing the right way
+    for n = 1, math.abs(zDel) do
+        moveForward()
+    end
 
-   -- Now x movement
-   if xDel > 0 then
-      while xDir() ~= 1 do
-         turnRight()
-      end
-   end
-   if xDel < 0 then
-      while xDir() ~= -1 do
-         turnRight()
-      end
-   end
-   -- Now it's facing the right way
-   for n=1,math.abs(xDel) do
-      moveForward()
-   end
+    -- Now x movement
+    if xDel > 0 then
+        if xDirIndex == 3 then
+            turnLeft()
+        else
+            while xDir() ~= 1 do
+                turnRight()
+            end
+        end
+    end
+    if xDel < 0 then
+        if xDirIndex == 1 then
+            turnLeft()
+        else
+            while xDir() ~= -1 do
+                turnRight()
+            end
+        end
+    end
+    -- Now it's facing the right way
+    for n = 1, math.abs(xDel) do
+        moveForward()
+    end
 end
 
 local function main()
-  -- Make sure there's a chest behind
---[[   if not checkChest() then
+    -- Make sure there's a chest behind
+    --[[   if not checkChest() then
     print("Please place a chest behind the turtle to begin.")
     return
   end ]]
 
-  -- Basic layer program
-  while y < yLimit do
-     for N=1,dim do
-        -- Figure out which side we're on. If we're on the origin side, go forward; otherwise, come back
-        if z == 0 then
-           moveTo(N - 1, dim)
-           moveTo(N, dim)
-        else
-           moveTo(N - 1, 0)
-           moveTo(N, 0)
-        end
+    -- Basic layer program
+    while y < yLimit do
+        for N = 1, dim do
+            -- Figure out which side we're on. If we're on the origin side, go forward; otherwise, come back
+            if z == 0 then
+                moveTo(N - 1, dim)
+                moveTo(N, dim)
+            else
+                moveTo(N - 1, 0)
+                moveTo(N, 0)
+            end
 
-     end
-     moveTo(0, 0)
-     moveUp()
-  end
-  while y > 0 do
-     moveDown()
-  end
+        end
+        moveTo(0, 0)
+        moveUp()
+    end
+    while y > 0 do
+        moveDown()
+    end
 end
 
 main()
